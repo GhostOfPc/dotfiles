13c8ed086ad50de089d2c174833d931c7fdf35b3
HishamAHai <hisham.ahai@gmail.com>
2020-06-06 16:47:46 -0300

    AwesomeWM apps declaration

---
+++ .config/awesome/apps.lua
@@ -0,0 +1,73 @@
+--    ██             ██         ████████ 
+--   ░██            ████       ██░░░░░░██
+--   ░██           ██░░██     ██      ░░ 
+--   ░██          ██  ░░██   ░██         
+--   ░██         ██████████  ░██    █████
+--   ░██        ░██░░░░░░██  ░░██  ░░░░██
+--   ░████████  ░██     ░██   ░░████████ 
+--   ░░░░░░░░   ░░      ░░     ░░░░░░░░  
+--        ██                                                             ██       ██
+--       ████                                                           ░██      ░██
+--      ██░░██   ███     ██  █████   ██████  ██████  ██████████   █████ ░██   █  ░██
+--     ██  ░░██ ░░██  █ ░██ ██░░░██ ██░░░░  ██░░░░██░░██░░██░░██ ██░░░██░██  ███ ░██
+--    ██████████ ░██ ███░██░███████░░█████ ░██   ░██ ░██ ░██ ░██░███████░██ ██░██░██
+--   ░██░░░░░░██ ░████░████░██░░░░  ░░░░░██░██   ░██ ░██ ░██ ░██░██░░░░ ░████ ░░████
+--   ░██     ░██ ███░ ░░░██░░██████ ██████ ░░██████  ███ ░██ ░██░░██████░██░   ░░░██
+--   ░░      ░░ ░░░    ░░░  ░░░░░░ ░░░░░░   ░░░░░░  ░░░  ░░  ░░  ░░░░░░ ░░       ░░ 
+--    ████     ████                                    
+--   ░██░██   ██░██             ██████  ██████         
+--   ░██░░██ ██ ░██    ██████  ░██░░░██░██░░░██  ██████
+--   ░██ ░░███  ░██   ░░░░░░██ ░██  ░██░██  ░██ ██░░░░ 
+--   ░██  ░░█   ░██    ███████ ░██████ ░██████ ░░█████ 
+--   ░██   ░    ░██   ██░░░░██ ░██░░░  ░██░░░   ░░░░░██
+--   ░██        ░██  ░░████████░██     ░██      ██████ 
+--   ░░         ░░    ░░░░░░░░ ░░      ░░      ░░░░░░  
+--    ╔════════════════════════════════════════╗
+--   ╔╝                                        ╚╗
+--   ║ Riced and crafted by  Hisham Abdul Hai  ║
+--   ║ ...Founder of Linux Arab Gate (L A G)... ║
+--   ╚╗                                        ╔╝ 
+--    ╚════════════════════════════════════════╝
+
+local awful = require('awful')
+local filesystem = require('gears.filesystem')
+
+local apps = {
+	browser = "brave",
+	editor	= os.getenv("EDITOR") or "nvim",
+	fmanager = "pcmanfm",
+	geditor	= "geany",
+	launcher = "rofi -normal-window -modi drun -show drun",
+	lock	= "i3lock",
+	mc	= "kodi",
+	mplayer	= "spotify",
+	screenshot	=	'maim -B -u $HOME/Pictures/"Screenshot-"$(date +%Y-%m-%d-%H-%M).png',
+	terminal = "kitty",
+	vplayer	= "vlc",
+	vector	= "inkscape"
+}
+local function run_once(cmd)
+    local findme = cmd
+    local firstspace = cmd:find(' ')
+    if firstspace then
+      findme = cmd:sub(0, firstspace - 1)
+    end
+    awful.spawn.with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', findme, cmd), false)
+end
+
+do
+  local startup_apps=
+  {
+    "nitrogen --restore",
+    "picom --experimental-backend",
+    "xfce4-power-manager",
+    "dunst",
+    "lxsession"
+  }
+
+  for _,apps in pairs(startup_apps) do
+    run_once(apps)
+  end
+end
+
+return apps
