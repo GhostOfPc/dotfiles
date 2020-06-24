f6557aa10b207cd91a47f25b5d863e12e5220580
HishamAHai <hisham.ahai@gmail.com>
2020-06-24 11:51:31 -0300

    minor edit

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -17,20 +17,20 @@ function send_notification {
   brightness=$(get_brightness)
   # Make the bar with the special character ─ (it's not dash -)
   # https://en.wikipedia.org/wiki/Box-drawing_character
-  bar=$(seq -s "▊" 0 $((brightness / 5)) | sed 's/[0-9]//g')
+  bar=$(seq -s "━" 0 $((brightness / 5)) | sed 's/[0-9]//g')
   # Send the notification
-  dunstify -i "$icon" -r 5555 -u normal "    $bar"
+  dunstify "$brightness"" %   ""$bar" -i "$icon" -h int:value:"$brightness" -r 444
 }
 
 case $1 in
   up)
-    # increase the backlight by 5%
-    xbacklight -inc 1
+    # increase the backlight by 2%
+    xbacklight -inc 2
     send_notification
     ;;
   down)
-    # decrease the backlight by 5%
-    xbacklight -dec 1
+    # decrease the backlight by 2%
+    xbacklight -dec 2
     send_notification
     ;;
 esac
