fbc21954cc63b69edd01d3ed339b680acbe0d232
HishamAHai <hisham.ahai@gmail.com>
2020-06-25 13:27:19 -0300

    removing not used items

--- .config/awesome/themes/theme.lua
+++ .config/awesome/themes/theme.lua
@@ -1,17 +1,17 @@
---    ╔════════════════════════════════════════╗
---   ╔╝                                        ╚╗
---   ║ Riced and crafted by  Hisham Abdul Hai  ║
---   ║ ...Founder of Linux Arab Gate (L A G)... ║
---   ╚╗                                        ╔╝ 
---    ╚════════════════════════════════════════╝
+--[[
+    ╔════════════════════════════════════════╗
+   ╔╝                                        ╚╗
+   ║ Riced and crafted by  Hisham Abdul Hai  ║
+   ║ ...Founder of Linux Arab Gate (L A G)... ║
+   ╚╗                                        ╔╝ 
+    ╚════════════════════════════════════════╝
+--]]
 
 local theme_assets = require('beautiful.theme_assets')
 local xresources = require('beautiful.xresources')
 local rnotification = require('ruled.notification')
 local dpi = xresources.apply_dpi
-
 local themes_path = '/home/hisham/.config/awesome/themes/'
---local themes_path = gears.filesystem.get_configuration_dir() ..'/themes/'
 
 local theme = {}
 
@@ -21,9 +21,8 @@ theme.font          =   'FiraMono Nerd Font 10'
 
 -- Background colors
 theme.bg_normal     =   '#181818'
-theme.bg_urgent     =   '#ff000040'
+theme.bg_urgent     =   '#ff92d0'
 theme.bg_focus      =   theme.bg_normal
-theme.bg_minimize   =   theme.bg_normal
 theme.bg_systray    =   theme.bg_normal
 
 -- Border colors
@@ -35,24 +34,17 @@ theme.border_focus      =   '#7575ff'
 theme.fg_normal     =   '#eeeeee'
 theme.fg_focus      =   theme.fg_normal
 theme.fg_urgent     =   theme.fg_normal
-theme.fg_minimize   =   theme.fg_normal
 
 -- fullscreen
 theme.fullscreen_hide_border = true
 
--- menubar
-theme.menubar_fg_normal     =   theme.fg_normal
-theme.menubar_bg_normal     =   theme.bg_normal
-theme.menubar_border_width  =   theme.border_width
-theme.menubar_border_color  =   theme.border_focus
-
 -- Notifications
-theme.notification_font         =   theme.font
-theme.notification_bg           =   theme.bg_normal
-theme.notification_fg           =   theme.fg_normal
-theme.notification_border_width =   theme.border_width
-theme.notification_border_color =   theme.border_focus
-theme.notification_opacity      =   0.8
+--theme.notification_font         =   theme.font
+--theme.notification_bg           =   theme.bg_normal
+--theme.notification_fg           =   theme.fg_normal
+--theme.notification_border_width =   theme.border_width
+--theme.notification_border_color =   theme.border_focus
+--theme.notification_opacity      =   0.8
 
 -- tooltip
 theme.tooltip_border_color  =   theme.border_focus
@@ -70,23 +62,15 @@ theme.wibar_border_width    =   dpi(0)
 theme.wibar_border_color    =   theme.bg_normal
 theme.wibar_opacity         =   0.85
 
--- menu
-theme.menu_font         =   theme.font
-theme.menu_width        =   dpi(150)
-theme.menu_border_color =   theme.border_focus
-theme.menu_border_width =   dpi(1)
-theme.menu_fg_focus     =   theme.fg_focus
-theme.menu_bg_focus     =   theme.bg_focus
-theme.menu_fg_normal    =   theme.fg_normal
-theme.menu_bg_normal    =   theme.bg_normal
-
 -- hotkeys
-theme.hotkeys_bg            =   theme.bg_normal
-theme.hotkeys_font          =   theme.font
-theme.hotkeys_fg            =   theme.fg_normal
-theme.hotkeys_border_width  =   dpi(1)
-theme.hotkeys_border_color  =   theme.border_focus
-theme.hotkeys_opacity	=	0.8
+theme.hotkeys_bg		=	theme.bg_normal
+theme.hotkeys_font		=	theme.font
+theme.hotkeys_fg		=	theme.fg_normal
+theme.hotkeys_border_width	=	dpi(1)
+theme.hotkeys_border_color	=	theme.border_focus
+theme.hotkeys_opacity		=	0.85
+theme.hotkeys_modifiers_fg	=	theme.border_focus 
+theme.hotkeys_description_font	=	theme.font
 
 -- tasklist
 theme.tasklist_fg_normal                        =       theme.fg_normal
@@ -99,14 +83,6 @@ theme.tasklist_font                             =       theme.font
 theme.tasklist_align                            =       left
 theme.tasklist_spacing                          =       dpi(5)
 
--- calendar\
--- theme.calendar\_style = nil
--- theme.calendar\_font = nil
--- theme.calendar\_spacing = nil
--- theme.calendar\_week\_numbers = nil
--- theme.calendar\_start\_sunday = nil
--- theme.calendar\_long\_weekdays = nil
-
 theme.layout_fairh = themes_path..'layouts/fairhw.png'
 theme.layout_fairv = themes_path..'layouts/fairvw.png'
 theme.layout_floating  = themes_path..'layouts/floatingw.png'
@@ -116,4 +92,13 @@ theme.layout_tilebottom = themes_path..'layouts/tilebottomw.png'
 theme.layout_tile = themes_path..'layouts/tilew.png'
 theme.layout_spiral  = themes_path..'layouts/spiralw.png'
 theme.layout_dwindle = themes_path..'layouts/dwindlew.png'
+
+-- calendar\
+-- theme.calendar\_style = nil
+-- theme.calendar\_font = nil
+-- theme.calendar\_spacing = nil
+-- theme.calendar\_week\_numbers = nil
+-- theme.calendar\_start\_sunday = nil
+-- theme.calendar\_long\_weekdays = nil
+
 return theme
