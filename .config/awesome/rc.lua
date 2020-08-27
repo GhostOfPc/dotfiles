374a714ad5d0ec877fb0ffc848aae9c227d98130
HishamAHai <hishamahai@github.com>
2020-08-27 10:22:48 -0300

    minor edits

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -12,7 +12,6 @@ local gears = require('gears')
 local awful = require('awful')
 local wibox = require('wibox')
 local beautiful = require('beautiful')
-local naughty = require('naughty')
 local menubar = require('menubar')
 require("awful.hotkeys_popup.keys")
 
@@ -28,29 +27,14 @@ require('keys')
 require('layouts')
 require('tags')
 require('top_bar')
+require('notifications')
 --require('bottom_bar')
 
--- ================================ Error handling ================================================
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
-end)
--- ================================ Error handling ================================================
-
--- ================================================================================================
 -- Enable sloppy focus, so that focus follows mouse.
-
 client.connect_signal('mouse::enter', function(c)
     c:activate { context = 'mouse_enter', raise = false }
 end)
 
--- ================================================================================================
 -- Garbage collection (allows for lower memory consumption)
--- ================================================================================================
 collectgarbage("setpause", 110)
 collectgarbage("setstepmul", 1000)
