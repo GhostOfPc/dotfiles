7f03aac4488d6b4a61846fcdd142e6781ea22d96
Hisham Abdul Hai <hisham.ahai@gmail.com>
2021-10-18 13:34:31 -0300

    changing maximum width

--- .config/dmenu/config.def.h
+++ .config/dmenu/config.def.h
@@ -8,20 +8,20 @@ static int colorprompt = 1;                /* -p  option; if 1, prompt uses Sche
 static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FantasqueSansMono Nerd Font:size=9",
-    "JoyPixels:size=9:antialias=true:hinting=true"
+	"FantasqueSansMono Nerd Font:size=10",
+    "JoyPixels:size=10:antialias=true:hinting=true"
 };
 static const unsigned int bgalpha = 0xc8;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#f5f5f5", "#212121"},
-	[SchemeSel] = { "#e5b566", "#505050" },
-	[SchemeSelHighlight] = { "#9e4e85", "#6c99ba" },
-	[SchemeNormHighlight] = { "#9e4e85", "#212121" },
-	[SchemeOut] = { "#151515", "#7e8d50" },
-	[SchemeHp] = { "#ac4142", "#505050" }
+	[SchemeNorm] = { "#bbbbbb", "#121212"},
+	[SchemeSel] = { "#fef26c", "#545454" },
+	[SchemeSelHighlight] = { "#af87ff", "#0f7fcf" },
+	[SchemeNormHighlight] = { "#f5669c", "#121212" },
+	[SchemeOut] = { "#121212", "#97e123" },
+	[SchemeHp] = { "#f5669c", "#545454" }
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
@@ -37,11 +37,11 @@ static const unsigned int alphas[SchemeLast][2] = {
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 27;
-static unsigned int min_lineheight = 27;
+static unsigned int lineheight = 40;
+static unsigned int min_lineheight = 40;
 static unsigned int xoffset = 5;
 static unsigned int yoffset = 3;
-static unsigned int max_width = 1910;
+static unsigned int max_width = 3830;
 
 /*
  * Characters not considered part of a word while deleting words
