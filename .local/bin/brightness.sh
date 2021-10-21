7f0168ec0767e0b953a1d15443b557346979e0e3
Hisham Abdul Hai <hisham.ahai@gmail.com>
2021-10-21 11:20:43 -0300

    minor changes

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-bright=$(ddccontrol -r 0x10 -W $110 dev:/dev/i2c-6 | awk '/^Control/ {print $3}' | awk -F "/" '{print $2}')
+set=$(ddcutil setvcp 10 $1 $2)
+bright=$(ddcutil getvcp 10 | cut -b 67-69)
 bar=$(seq -s "🬋" $(($bright/5)) | sed 's/[0-9]//g')
 
-$HOME/.local/bin/notify-send.py $bright"% "$bar -i "/usr/share/icons/Papirus/48x48/status/notification-display-brightness-full.svg" --replaces-process brightness
+notify-send.py $bright"% "$bar -i "/usr/share/icons/Papirus/48x48/status/notification-display-brightness-full.svg" --replaces-process brightness
