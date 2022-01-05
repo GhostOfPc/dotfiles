584e4df7bf40933ae2f5407e059d733cad1a353d
Hisham Abdul Hai <hishamabdulhai@protonmail.com>
2022-01-05 20:56:24 -0300

    refactoring the sed command

--- .local/bin/awesome_theme.sh
+++ .local/bin/awesome_theme.sh
@@ -3,14 +3,12 @@
 theme=$(ls ~/.config/awesome/themes/colorschemes | dmenu -i -l 5 -g 10 -p "Select a theme: ")
 
 [ -z "$theme" ] && exit
-
 sed -i "17s/'.*'/'$theme'/" ~/.config/awesome/themes/theme.lua
 
 if [[ $theme == *"Dark"* ]] || [[ $theme == *"Night"* ]] || [[ $theme == "Molokai" ]] || [[ $theme == "Argonaut" ]] || [[ $theme == "Encom" ]]; then
-    sed -i '4s/light/dark/' ~/.gtkrc-2.0
-    sed -i '5s/Light/Dark/' ~/.gtkrc-2.0
-    sed -i '2s/light/dark/' ~/.config/gtk-3.0/settings.ini
-    sed -i '3s/Light/Dark/' ~/.config/gtk-3.0/settings.ini
+    sed -i '6s/Light/Dark/ ; 7s/light/dark/' ~/.config/awesome/widgets/WEATHER_WIDGET.lua
+    sed -i '4s/light/dark/ ; 5s/Light/Dark/' ~/.gtkrc-2.0
+    sed -i '2s/light/dark/ ; 3s/Light/Dark/' ~/.config/gtk-3.0/settings.ini
     printf "[General]\ntheme=VimixRubyDark" > ~/.config/Kvantum/kvantum.kvconfig
     sed -i '4s/Light/Dark/' ~/.config/qt5ct/qt5ct.conf
     sed -i '24s/lighter/darker/' ~/.config/nvim/init.vim
@@ -22,10 +20,9 @@ if [[ $theme == *"Dark"* ]] || [[ $theme == *"Night"* ]] || [[ $theme == "Moloka
     sudo make clean install
 
 else
-    sed -i '4s/dark/light/' ~/.gtkrc-2.0
-    sed -i '5s/Dark/Light/' ~/.gtkrc-2.0
-    sed -i '2s/dark/light/' ~/.config/gtk-3.0/settings.ini
-    sed -i '3s/Dark/Light/' ~/.config/gtk-3.0/settings.ini
+    sed -i '6s/Dark/Light/ ; 7s/dark/light/' ~/.config/awesome/widgets/WEATHER_WIDGET.lua
+    sed -i '4s/dark/light/ ; 5s/Dark/Light/' ~/.gtkrc-2.0
+    sed -i '2s/dark/light/ ; 3s/Dark/Light/' ~/.config/gtk-3.0/settings.ini
     printf "[General]\ntheme=VimixRuby" > ~/.config/Kvantum/kvantum.kvconfig
     sed -i '4s/Dark/Light/' ~/.config/qt5ct/qt5ct.conf
     sed -i '24s/darker/lighter/' ~/.config/nvim/init.vim
