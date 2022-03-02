77227b653acbfc8c2264824b5638769563f2708b
Hisham Abdul Hai <hishamabdulhai@protonmail.com>
2022-03-02 20:59:36 -0300

    minor edit

--- .local/bin/awesome_theme.sh
+++ .local/bin/awesome_theme.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-theme=$(ls ~/.config/awesome/themes/colorschemes | dmenu -i -l 5 -g 10 -p "Select a theme: ")
+theme=$(ls ~/.config/awesome/themes/colorschemes | sed -e 's/\.conf//' | dmenu -i -l 5 -g 10 -p "Select a theme: ")
 
 [ -z "$theme" ] && exit
-sed -i "17s/'.*'/'$theme'/" ~/.config/awesome/themes/theme.lua
+sed -i "17s/'.*' .. '.conf'/'$theme' .. '.conf'/" ~/.config/awesome/themes/theme.lua
 
-if [[ $theme == *"Dark"* ]] || [[ $theme == *"Night"* ]] || [[ $theme == "Molokai" ]] || [[ $theme == "Argonaut" ]] || [[ $theme == "Encom" ]]; then
+if [[ $theme == *"Dark"* ]] || [[ $theme == *"Night"* ]] || [[ $theme == "Molokai" ]] || [[ $theme == "Argonaut" ]] || [[ $theme == "Encom" ]] || [[ $theme == "doom-one" ]]; then
     sed -i '6s/Light/Dark/ ; 7s/light/dark/' ~/.config/awesome/widgets/WEATHER_WIDGET.lua
     sed -i '4s/light/dark/ ; 5s/Light/Dark/' ~/.gtkrc-2.0
     sed -i '2s/light/dark/ ; 3s/Light/Dark/' ~/.config/gtk-3.0/settings.ini
     printf "[General]\ntheme=VimixRubyDark" > ~/.config/Kvantum/kvantum.kvconfig
     sed -i '4s/Light/Dark/' ~/.config/qt5ct/qt5ct.conf
     sed -i '24s/lighter/darker/' ~/.config/nvim/init.vim
-    kitty +kitten themes Material Dark
+    kitty +kitten themes Doom One
 
     cd ~/.config/dmenu
     rm -f config.h
@@ -26,7 +26,7 @@ else
     printf "[General]\ntheme=VimixRuby" > ~/.config/Kvantum/kvantum.kvconfig
     sed -i '4s/Dark/Light/' ~/.config/qt5ct/qt5ct.conf
     sed -i '24s/darker/lighter/' ~/.config/nvim/init.vim
-    kitty +kitten themes Material
+    kitty +kitten themes Doom One Light
 
     cd ~/.config/dmenu
     rm -f config.h
